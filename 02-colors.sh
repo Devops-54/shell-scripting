9166b14c5d52dfcd1ac310945aae8bb778703fb6
Uttej uv <sushprav11@gmail.com>
2023-06-26 19:21:39 +0530

    Auto Push

--- 02-colors.sh
+++ 02-colors.sh
@@ -26,4 +26,8 @@ echo -e "\e[32m I am printing Green color \e[0m"
 echo -e "\e[33m I am printing Yellow color \e[0m"
 echo -e "\e[34m I am printing Blue color \e[0m"
 echo -e "\e[35m I am printing Magenta color \e[0m"
-echo -e "\e[36m I am printing Cyan color \e[om"
+echo -e "\e[36m I am printing Cyan color \e[0m"
+
+# Printing Background color
+
+echo -e "\e[43;32m I am printing Green color with yellow Background \e[0m"
