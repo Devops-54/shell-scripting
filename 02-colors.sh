b9f4ccb04b1643519de80aa02839e93d71d61412
Uttej uv <sushprav11@gmail.com>
2023-06-26 19:14:12 +0530

    Auto Push

--- 02-colors.sh
+++ 02-colors.sh
@@ -22,8 +22,8 @@
  #      echo -e "\e[COL-CODEm  Your Message To Be Printed \e[0m"
  #      echo -e "\e[32m I am printing Green Color \e[0m"
 
-echo -e "\e[32m I am printing Green color \e[om"
-echo -e "\e[33m I am printing Yellow color \e[om"
-echo -e "\e[34m I am printing Blue color \e[om"
-echo -e "\e[35m I am printing Magenta color \e[om"
+echo -e "\e[32m I am printing Green color \e[0m"
+echo -e "\e[33m I am printing Yellow color \e[0m"
+echo -e "\e[34m I am printing Blue color \e[0m"
+echo -e "\e[35m I am printing Magenta color \e[0m"
 echo -e "\e[36m I am printing Cyan color \e[om"
