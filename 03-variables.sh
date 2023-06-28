f11fa31575562996866fb00f5f3ea31f5e0d6e56
Uttej uv <sushprav11@gmail.com>
2023-06-28 19:27:03 +0530

    Auto Push

---
+++ 03-variables.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+# What is a variable ? Variable is used to store and pass values
+# a = 10
+# name=Verma
+# batch=54
+# topic=shellscripting
+
+# In bash there is no concept of Data Types : By default everything is a string
+# int a = 10 
+# string b = Manu 
+# float c = 0.15
+
+# This is how we can declare variables in Bash 
+a=10 
+b=20
+c=30
+
+# Syntax : $variableName : $ is going to print the value of the variable
+echo $a
+echo value of the variable is $a
