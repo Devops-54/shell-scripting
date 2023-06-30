344bb2dd150d006deae7418e6ad5a3b1e87c099d
Uttej uv <sushprav11@gmail.com>
2023-06-30 14:57:37 +0530

    Auto Push

--- 04-dynamicVars.sh
+++ 04-dynamicVars.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# TODAYS_DATE="20JUNE2023"                            # HardCoding a value makes it consistent
+TODAYS_DATE=$(date +%F)              
 echo "Good Morning and todays date is 20June2023"
