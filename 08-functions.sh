e597e2d7df7a6a9ab2d5a842aa8ea34fee7beaff
Uttej uv <sushprav11@gmail.com>
2023-06-30 18:50:31 +0530

    Auto Push

--- 08-functions.sh
+++ 08-functions.sh
@@ -11,9 +11,16 @@ sample() {
     echo "I am a sample function with name sample"
     echo "I am executing the sample function"
     echo "sample function is completed"
+}
 
+status() {
+    echo -e "Good Morning and todays date is \e[32m $(date +%F)  \e[0m"
+    echo -e "Number of opened sessions : \e[32m $(who | wc -l) \e[0m"    
 }
 
 # This is now you can call a function
 
 sample
+
+# calling status function
+status
