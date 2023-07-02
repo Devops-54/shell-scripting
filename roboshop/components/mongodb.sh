29283121366f5154ea110fd79a322f161872e768
Uttej uv <sushprav11@gmail.com>
2023-07-02 20:57:24 +0530

    Auto push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -27,10 +27,16 @@ echo -n "Installing $COMPONENT : "
 yum install -y $COMPONENT-org  &>> $LOGFILE
 stat $?
 
+echo -n "Enabling the DB visibility :"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+stat $?
+
 echo -n "Starting $COMPONENT : "
 systemctl enable mongod      &>> $LOGFILE
 systemctl restart mongod     &>> $LOGFILE
 stat $?
+
+
 
 
 
