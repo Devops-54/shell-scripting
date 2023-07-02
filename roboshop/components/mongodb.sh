c8a5d25f7afcaa20149557f21c8d9b86622eff6b
Uttej uv <sushprav11@gmail.com>
2023-07-02 21:22:58 +0530

    Auto push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -19,6 +19,8 @@ stat() {
     fi
 }
 
+echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
+
 echo -n  "Configuring the $COMPONENT repo :"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 stat $?
@@ -51,6 +53,9 @@ cd $COMPONENT-main
 mongo < catalogue.js       &>> $LOGFILE 
 mongo < users.js           &>> $LOGFILE 
 stat $?
+
+
+echo -e "*********** \e[35m $COMPONENT Installation is completed \e[0m ***********"
 
 
 
