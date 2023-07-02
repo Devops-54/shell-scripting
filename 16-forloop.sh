3ce5db9243aaa12c52150cd670b60b06d2a0e1aa
Uttej uv <sushprav11@gmail.com>
2023-07-02 16:01:05 +0530

    Auto push

---
+++ 16-forloop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# What is a loop ?
+# If you want to execute something certain number of times, the we use loop   
+
+# for loop is a very famous loop when you know whole many number of times, we need to to run. 
+
+# For loop syntax : 
+for val in x y z 1 2 3 ; do
+    echo "value of variable is $val"
+done
