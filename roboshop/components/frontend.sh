dc6a7685ee47ec643dbd7ea806d0c7e47a84e89b
Uttej uv <sushprav11@gmail.com>
2023-07-02 17:38:58 +0530

    Auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,6 +2,13 @@
 
 COMPONENT=frontend
 
+ID=$(id -u)
+
+if [ $ID -ne 0 ] ; then
+    echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilege \e[0m"
+    exit 1
+fi 
+
 echo "Installing Nginx :"
 yum install nginx -y  &>> "/tmp/${COMPONENT}.log"
 
