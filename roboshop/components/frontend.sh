ca715995bbbeca1fa882646fdb6c3b286050f710
Uttej uv <sushprav11@gmail.com>
2023-07-06 19:54:52 +0530

    Auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -25,7 +25,7 @@ echo "Installing Nginx :"
 yum install nginx -y  &>> $LOGFILE
 stat $?
 
-echo -n "Downloading the frontend component  :"
+echo -n "Downloading the ${COMPONENT} component  :"
 curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
 
