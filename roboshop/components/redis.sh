043bcb8a92d01f10553b0e6ff4e06da3990c8c43
Uttej uv <sushprav11@gmail.com>
2023-07-07 17:43:39 +0530

    Auto push

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -8,21 +8,21 @@ echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m **********
 
 echo -n  "Configuring the $COMPONENT repo :"
 curl -s -L https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/${COMPONENT}.repo -o /etc/yum.repos.d/${COMPONENT}.repo
-stat $?
+stat $? 
 
 echo -n "Installing $COMPONENT : "
-yum install -y ${COMPONENT}-6.2.11  &>> $LOGFILE
-stat $?
+yum install -y ${COMPONENT}-6.2.11   &>> $LOGFILE
+stat $? 
 
 echo -n "Enabling the DB visibility :"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/${COMPONENT}.conf
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/${COMPONENT}/${COMPONENT}.conf
-stat $?
+stat $? 
 
 echo -n "Starting $COMPONENT : "
 systemctl daemon-reload $COMPONENT      &>> $LOGFILE
-systemctl enable $COMPONENT     &>> $LOGFILE
-systemctl restart $COMPONENT     &>> $LOGFILE
+systemctl enable $COMPONENT      &>> $LOGFILE
+systemctl restart $COMPONENT       &>> $LOGFILE
 stat $?
 
 echo -e "*********** \e[35m $COMPONENT Installation is completed \e[0m ***********"
