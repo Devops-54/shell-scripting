2fe4ff23a59a2610effff5e7c4fe76d4c50099c1
Uttej uv <sushprav11@gmail.com>
2023-07-08 15:54:23 +0530

    Auto push

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -121,3 +121,28 @@ JAVA() {
     CONFIGURE_SVC
 
 }
+
+
+PYTHON() {
+    echo -n "Installing Python and its dependencies :"
+    yum install python36 gcc python3-devel -y   &>> $LOGFILE 
+    stat $? 
+
+    CREATE_USER                 # calling Create_user function to create the roboshop user account
+
+    DOWNLOAD_AND_EXTRACT         # calling DOWNLOAD_AND_EXTRACT  function download the content
+
+    echo -n "Installing $COMPONENT"
+    cd /home/${APPUSER}/${COMPONENT}/
+    pip3 install -r requirements.txt    &>> $LOGFILE 
+    stat $?
+
+    USERID=$(id -u roboshop)
+    GROUPID=$(id -g roboshop) 
+
+    echo -n "Updating the uid and gid in the $COMPONENT.ini file"
+    sed -i -e "/^uid/ c uid=${USERID}" -e "/^gid/ c gid=${GROUPID}"  /home/${APPUSER}/${COMPONENT}/${COMPONENT}.ini
+    
+    CONFIGURE_SVC
+
+}
