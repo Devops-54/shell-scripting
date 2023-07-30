9a99cdf321957ec89cf5933c02ce13164284fd3a
Uttej uv <sushprav11@gmail.com>
2023-07-30 10:36:04 +0530

    Updated the readme file and corrected gramatically

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -35,15 +35,4 @@ create_ec2() {
 }
 
 
-if [ "$1" = "all" ]; then    
-
-    for component in frontend mongodb catalogue redis user cart shipping mysql rabbitmq payment ; do 
-        COMPONENT=$component
-        create_ec2
-    done 
-
-else 
-
-    create_ec2
-
-fi 
+ 
